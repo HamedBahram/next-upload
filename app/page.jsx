0d43f964afbf003f61d3060af3466e3f5bf93ed7
Hamed Bahram <hamed.bahram@gmail.com>
2023-06-03 11:17:45 -0400

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Hello World</h1>
+      </div>
+    </section>
+  )
+}
